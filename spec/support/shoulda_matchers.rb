7052aff3580aca3b5be27addde35ad531c2fc478
Pablo Salgado <pabloasalgado@gmail.com>
2026-04-15 12:45:38 -0500

    Add Shoulda Matchers support for RSpec testing

--- spec/support/shoulda_matchers.rb
+++ spec/support/shoulda_matchers.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-RSpec.configure do |config|
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
   end
 end
