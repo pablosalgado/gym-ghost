0ded0d91f2195f18c640ed5539395212e697c698
pablosalgado <pabloasalgado@gmail.com>
2026-06-18 13:35:38 -0500

    Chore: localize UI to Spanish via I18n (#76)
    * Chore: localize UI to Spanish via I18n
    
    * Fix: use I18n.l for day abbreviations in date strip

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
   before_action :redirect_if_authenticated, only: %i[ new ]
-  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: "Try again later." }
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_path, alert: t("flash.try_again") }
 
   def new
   end
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       start_new_session_for user
       redirect_to after_authentication_url
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      redirect_to new_session_path, alert: t("flash.invalid_credentials")
     end
   end
 
