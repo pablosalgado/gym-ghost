7f3011653c55313a6705233edc3ca06550dc4a7f
pablosalgado <pabloasalgado@gmail.com>
2026-07-18 13:55:18 -0500

    Remove attr_encrypted initializer - dotenv-rails loads key from .env

--- config/initializers/attr_encrypted.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-key = Rails.application.credentials.attr_encrypted_key || ENV["ATTR_ENCRYPTED_KEY"]
-
-if key.nil? && Rails.env.production?
-  raise "ATTR_ENCRYPTED_KEY must be set in credentials or ENV for production"
-end
-
-ENV["ATTR_ENCRYPTED_KEY"] = key if key
