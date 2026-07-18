5401fa0fa47c1050e15d3c2433c4d50a2fc0559f
pablosalgado <pabloasalgado@gmail.com>
2026-07-18 13:30:25 -0500

    Remove hardcoded fallback encryption key from boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -2,5 +2,3 @@ ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
-
-ENV["ATTR_ENCRYPTED_KEY"] ||= "a]3$7!xK9#mP2vQ8wR5tY0bN4dF6gH1j"
