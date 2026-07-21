85e930f16b8e9161b03ddf21622f374cb6d38fea
pablosalgado <pabloasalgado@gmail.com>
2026-07-21 15:34:03 -0500

    Use strong params for city_id filter in FacilitiesController

--- app/controllers/api/v1/facilities_controller.rb
+++ app/controllers/api/v1/facilities_controller.rb
@@ -3,9 +3,15 @@ module Api
     class FacilitiesController < ApplicationController
       def index
         facilities = Facility.order(:display_name)
-        facilities = facilities.where(city_id: params[:city_id]) if params[:city_id].present?
+        facilities = facilities.where(city_id: filter_params[:city_id]) if filter_params[:city_id].present?
 
         render json: { facilities: facilities.as_json(only: [ :id, :display_name, :city_id ]) }
+      end
+
+      private
+
+      def filter_params
+        params.permit(:city_id)
       end
     end
   end
